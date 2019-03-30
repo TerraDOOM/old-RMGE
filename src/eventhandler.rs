94ecc1cb3711d3fd253314428d415ee802faa8e5
TerraDOOM <look4totte@gmail.com>
2019-03-30 01:30:15 +0100

    added some extra files that I forgot about before

---
+++ src/eventhandler.rs
@@ -0,0 +1,64 @@
+use std::time::Instant;
+use winit::ButtonId;
+
+impl From<(Instant, winit::Event)> for Event {
+    fn from(arg: (Instant, winit::Event)) -> Event {
+        let (time, event) = arg;
+        Event {
+            time,
+            event: RMEvent::from(event)
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Event {
+    pub time: Instant,
+    pub event: RMEvent,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum RMEvent {}
+
+impl From<winit::Event> for RMEvent {
+    fn from(_event: winit::Event) -> RMEvent {
+        unimplemented!()
+    }
+}
+
+/// Internal struct for mapping WindowEvents to easier-to-handle game events (I think for now at least)
+#[derive(Debug, Clone)]
+pub struct RMEventHandler<E: EventHandler> {
+    event_handler: E,
+    button_map: ButtonMap,    
+}
+
+impl<E: EventHandler> RMEventHandler<E> {
+    pub fn new(_e: E) -> Self {
+        unimplemented!()
+    }
+    
+    pub fn handle_event(&mut self, event: Event) {
+        // do some processing on the event... 
+        self.event_handler.handle_event(event)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum VirtualKeyCode {}
+
+#[derive(Debug, Clone)]
+struct ButtonMap {
+    map: Vec<Option<VirtualKeyCode>>,
+}
+
+impl ButtonMap {
+    #[allow(dead_code)]
+    fn map_button(&self, button: ButtonId) -> Option<VirtualKeyCode> {
+        (self.map.get(button as usize).cloned()).unwrap_or(None)
+    }
+}
+
+pub trait EventHandler {
+    fn handle_event(&mut self, event: Event);
+}
