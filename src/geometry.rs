bb9f2325965967bfa1916dc9cae3e818c6a3820c
TerraDOOM <look4totte@gmail.com>
2019-03-30 22:07:07 +0100

    deleted a lot of code that wasn't being used

--- src/geometry.rs
+++ src/geometry.rs
@@ -19,7 +19,7 @@ impl Rectangle {
     }
 }
 
-/// Quad of points stored as: Top left, bottom left, bottom right, top right
+/// Quad of points
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct Quad {
@@ -30,46 +30,7 @@ pub struct Quad {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Triangle {
-    points: [Point2D; 3],
-}
-
-impl Quad {
-    /*pub fn vertex_attributes(self) -> [Vertex; 4] {
-        let Quad {
-        top_left,
-        bottom_left,
-        bottom_right,
-        top_right,
-    } = self;
-        #[cfg_attr(rustfmt, rustfmt_skip)]
-        [/*
-        X               Y               R    G    B                  U    V                    */
-    top_left.x,     top_left.y,     1.0, 0.0, 0.0, /* red     */ 0.0, 1.0, /* bottom left  */
-    bottom_left.x,  bottom_left.y,  0.0, 1.0, 0.0, /* green   */ 0.0, 0.0, /* top left     */
-    bottom_right.x, bottom_right.y, 0.0, 0.0, 1.0, /* blue    */ 1.0, 0.0, /* bottom right */
-    top_right.x,    top_right.y,    1.0, 0.0, 1.0, /* magenta */ 1.0, 1.0, /* top right    */
-    ]
-    }*/
-}
-
-#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Point2D {
     pub x: f32,
     pub y: f32,
-}
-
-impl Into<[f32; 2]> for Point2D {
-    #[inline]
-    fn into(self) -> [f32; 2] {
-        [self.x, self.y]
-    }
-}
-
-impl From<[f32; 2]> for Point2D {
-    #[inline]
-    fn from(arr: [f32; 2]) -> Point2D {
-        let [x, y] = arr;
-        Point2D { x, y }
-    }
 }
