3ac573bb52e75235dce4d291b3da6d69f7eb5e2d
TerraDOOM <look4totte@gmail.com>
2019-04-01 21:45:38 +0200

    fixed some minor things in the event API

--- src/event.rs
+++ src/event.rs
@@ -4,11 +4,11 @@ use winit::{ButtonId, DeviceId, MouseScrollDelta, ScanCode, VirtualKeyCode};
 pub trait EventHandler {
     fn draw(&mut self);
     fn update(&mut self);
-    fn keyboard_down(&mut self, _time: Instant, _key: Key, _modifiers: KeyModifiers) {}
-    fn keyboard_up(&mut self, _time: Instant, _key: Key, _modifiers: KeyModifiers) {}
+    fn key_down(&mut self, _time: Instant, _key: Key) {}
+    fn key_up(&mut self, _time: Instant, _key: Key) {}
     fn device_button_down(&mut self, _time: Instant, _button: DeviceButton) {}
     fn device_button_up(&mut self, _time: Instant, _button: DeviceButton) {}
-    fn mouse_motion(&mut self, _time: Instant, _motion: MouseMotion) {}
+    fn mouse_move(&mut self, _time: Instant, _motion: MouseMove) {}
     fn mouse_wheel(&mut self, _time: Instant, _scroll: MouseScrollDelta) {}
     /// This function is run whenever the user changes focus. The return value is whether to suspend the event loop while unfocused.
     /// Default is to suspend the eventloop
@@ -20,7 +20,7 @@ pub trait EventHandler {
     }
 }
 
-pub struct MouseMotion {/* no fields yet */}
+pub struct MouseMove {/* no fields yet */}
 
 pub struct DeviceButton {
     pub device: DeviceId,
@@ -31,6 +31,7 @@ pub struct Key {
     pub device: DeviceId,
     pub scancode: ScanCode,
     pub virtual_keycode: Option<VirtualKeyCode>,
+    pub modifiers: KeyModifiers,
 }
 
 pub struct KeyModifiers {
